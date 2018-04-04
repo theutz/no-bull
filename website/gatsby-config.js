cfe6e32f7ff8ad0513b157bfd3107f75a12443b8
Michael Utz <michael.utz@nordsafety.com>
2018-04-04 13:36:51 +0300

    chore: added new version of react and relative module resolution

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     title: 'No Bull',
     description: 'Bulma + Styled Components = No Bull',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-resolve-src`,
+    `gatsby-plugin-react-next`,
+  ],
 }
